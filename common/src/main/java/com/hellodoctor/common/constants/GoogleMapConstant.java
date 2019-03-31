82de3d444b53a2766afb20ba641a633b06002bf9
Tran Ngoc Khoa <tranngoc.khoa.spk@gmail.com>
2019-03-31 14:58:22 +0700

    update

--- common/src/main/java/com/hellodoctor/common/constants/GoogleMapConstant.java
+++ common/src/main/java/com/hellodoctor/common/constants/GoogleMapConstant.java
@@ -6,7 +6,7 @@ package com.hellodoctor.common.constants;
  */
 public class GoogleMapConstant {
     public static final String URL_GEOCODING = "https://maps.googleapis.com/maps/api/geocode/";
-    public static final String API_KEY = "AIzaSyANDnuX1SK9oYTpKPUYw_drFllxSrBu-JU";
+    public static final String API_KEY = "INSERT HERE";
     public static final String JSON_OUTPUT = "json";
 
 
