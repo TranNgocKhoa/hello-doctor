65118bd78bae176a095dd4d6c29a5573a62431ae
Tran Ngoc Khoa <tranngoc.khoa.spk@gmail.com>
2019-04-11 23:26:38 +0700

    Add clould run

---
+++ pushAllImages.bat
@@ -0,0 +1,6 @@
+docker login
+docker push tranngockhoa/hellodoctor-discovery-server:latest
+docker push tranngockhoa/hellodoctor-api-gateway:latest
+docker push tranngockhoa/hellodoctor-configserver:latest
+docker push tranngockhoa/hellodoctor-location-service:latest
+docker push tranngockhoa/hellodoctor-booking-service:latest
